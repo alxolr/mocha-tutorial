99c08e33fa6351dc34c8234b85d5e9180788d83e
Alexandru Olaru <alxolr@gmail.com>
2018-11-18 00:30:06 +0200

    added the show hooks example code

---
+++ hooks/before.ts
@@ -0,0 +1,3 @@
+before(() => {
+  console.log('Hooks level');
+});
---
+++ hooks/first/second-level/some.test.ts
@@ -0,0 +1,7 @@
+import assert from 'assert';
+
+describe('Some simple test', () => {
+  it('should return true', () => {
+    assert.strictEqual(true, true);
+  });
+});
